bf0dc8ee618f5817314af072c11cf22c5c29ef91
Mikhail Persikov <gt.gluck@gmail.com>
2022-07-17 12:29:17 +0300

    Added reselect into the project and created filteredHeroesSelector function into HeroesList

--- hero_admin_panel/src/components/heroesList/HeroesList.js
+++ hero_admin_panel/src/components/heroesList/HeroesList.js
@@ -2,6 +2,7 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import { createSelector } from 'reselect';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
@@ -10,14 +11,30 @@ import Spinner from '../spinner/Spinner';
 import './heroesList.scss';
 
 const HeroesList = () => {
-    const filteredHeroes = useSelector(state => {
-        if(state.activeFilter === 'all') {
-            return state.heroes;
-        } else {
-            return state.heroes.filter(item => item.element === state.activeFilter)
-        }
-    })
-    const heroesLoadingStatus = useSelector(state => state);
+
+    const filteredHeroesSelector = createSelector(
+        state => state.filters.activeFilter,
+        state => state.heroes.heroes,
+        (filter, heroes) => {
+            if (filter === 'all') {
+                console.log('render');
+                return heroes;
+            } else {
+                return heroes.filter(item => item.element === filter)
+            }
+        } 
+    )
+
+    // const filteredHeroes = useSelector(state => {
+    //     if(state.filters.activeFilter === 'all') {
+    //         return state.heroes.heroes;
+    //     } else {
+    //         return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
+    //     }
+    // })
+
+    const filteredHeroes = useSelector(filteredHeroesSelector)
+    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
